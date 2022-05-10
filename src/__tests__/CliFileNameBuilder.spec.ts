947d5f82e4e69b4341e77d8304fc004b423961b9
fabasoad <fabasoad@gmail.com>
2022-05-10 15:58:43 +0900

    Fix URL

--- src/__tests__/CliFileNameBuilder.spec.ts
+++ src/__tests__/CliFileNameBuilder.spec.ts
@@ -5,30 +5,16 @@ import { TOOL_NAME } from '../consts'
 
 jest.mock('os', () => ({ type: jest.fn() }))
 
-interface IFixture {
-  os1: string
-  os2: string
-}
-
 describe('CliFileNameBuilder', () => {
   const expectedVersion: string = 'dy79bl7s'
-  const items: IFixture[] = [{
-    os1: 'Windows_NT',
-    os2: 'windows'
-  }, {
-    os1: 'Darwin',
-    os2: 'mac'
-  }, {
-    os1: 'Linux',
-    os2: 'linux'
-  }]
+  const items: string[] = ['Darwin', 'Linux']
 
-  itParam('should build successfully (${value.os1})',
-    items, (item: IFixture) => {
-      (type as jest.Mock).mockImplementation(() => item.os1)
+  itParam('should build successfully (${value})',
+    items, (item: string) => {
+      (type as jest.Mock).mockImplementation(() => item)
       const b: CliFileNameBuilder = new CliFileNameBuilder(expectedVersion)
       expect(b.build()).toBe(
-        `${TOOL_NAME}_v${expectedVersion}_${item.os2}_amd64`)
+        `${TOOL_NAME}_v${expectedVersion}_${item.toLowerCase()}_amd64`)
     })
 
   afterEach(() => (type as jest.Mock).mockClear())
