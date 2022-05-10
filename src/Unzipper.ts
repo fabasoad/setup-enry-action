0c2e94c3f7b78ac867dcd62032facccce4730669
fabasoad <fabasoad@gmail.com>
2022-05-10 16:03:32 +0900

    Fix tar gz

--- src/Unzipper.ts
+++ src/Unzipper.ts
@@ -1,19 +1,19 @@
-import { extractZip } from '@actions/tool-cache'
+import { extractTar } from '@actions/tool-cache'
 import path from 'path'
 import { Logger } from 'winston'
 import LoggerFactory from './LoggerFactory'
 
 export default class Unzipper implements IUnzipper {
-  private ez: typeof extractZip
+  private readonly untar: typeof extractTar
   private log: Logger
 
-  constructor(ez: typeof extractZip = extractZip) {
-    this.ez = ez
+  constructor(untar: typeof extractTar = extractTar) {
+    this.untar = untar
     this.log = LoggerFactory.create('Unzipper')
   }
 
   async unzip(zipPath: string): Promise<string> {
-    const folderPath: string = await this.ez(zipPath, path.dirname(zipPath))
+    const folderPath: string = await this.untar(zipPath, path.dirname(zipPath))
     this.log.info(`Unzipped ${zipPath} to ${folderPath}`)
     return folderPath
   }
