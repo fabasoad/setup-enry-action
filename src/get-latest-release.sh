098c4a6f3eb371f877fc4447840f6ffc4a64f580
fabasoad <fabasoad@gmail.com>
2026-07-02 17:47:32 +0900

    fix: version

--- src/get-latest-release.sh
+++ src/get-latest-release.sh
@@ -7,7 +7,7 @@ main() {
   version=$(curl -sL \
     -H "Accept: application/vnd.github+json" \
     -H "Authorization: Bearer ${token}" \
-    -H "X-GitHub-Api-Version: 2022-11-28" \
+    -H "X-GitHub-Api-Version: 2026-03-10" \
     "https://api.github.com/repos/${repo}/tags" \
     | jq -r '.[0].name | sub("^v"; "")')
   echo "version=${version}" >> "$GITHUB_OUTPUT"
